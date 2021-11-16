af49bb08844abc453df0d18f82b548cc41bc5d99
AbhishekVerma2021 <2001avverma@gmail.com>
2021-11-16 17:38:42 +0530

    Multi_02

--- src/Multi_01.java
+++ src/Multi_01.java
@@ -1,4 +1,19 @@
 public class Multi_01
 {
-    
+    public static void main(String[] args) {
+        Thread obj =Thread.currentThread();
+        System.out.println(obj.getName());
+        obj.setName("VERMA");
+        System.out.println(obj.getId());
+        System.out.println(obj.isAlive());
+        System.out.println(obj.getPriority());
+        System.out.println(obj.isDaemon());
+    }
 }
+//how to create multithreaded application
+//two ways to create multithreaded application
+//By the thread class
+//It is known as functional interface
+//one method
+//run() method
+//lamda expression
